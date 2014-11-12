94e2c6fc18bc12e702d132acc7de85d140be0c7c
ko <schaffer.krisztian@gmail.com>
2014-11-12 16:14:17 +0100

    platform dir removed from git

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Volumes/ko/projects/pentaschool/phonegap/phonegap-1/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Volumes/ko/projects/pentaschool/phonegap/phonegap-1/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/PhoneGap1-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Volumes/ko/projects/pentaschool/phonegap/phonegap-1/platforms/android/ant-build/PhoneGap1-debug-unaligned.apk : \
-/Volumes/ko/projects/pentaschool/phonegap/phonegap-1/platforms/android/ant-build/PhoneGap1.ap_ \
-/Volumes/ko/projects/pentaschool/phonegap/phonegap-1/platforms/android/ant-build/classes.dex \
-/Volumes/ko/projects/pentaschool/phonegap/phonegap-1/platforms/android/CordovaLib/ant-build/classes.jar \
